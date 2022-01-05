26ffaa0a85579bcc8d77b5b31e885d0b453df0e1
István Káplár <pistike.kaplar@gmail.com>
2022-01-05 12:38:36 +0100

    Added GameWindow style

--- Battleship/Battleship/obj/Debug/net5.0-windows/PlayerNameWindow.g.i.cs
+++ Battleship/Battleship/obj/Debug/net5.0-windows/PlayerNameWindow.g.i.cs
@@ -71,13 +71,13 @@ namespace Battleship {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.13.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.9.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Battleship;component/playernamewindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Battleship;V1.0.0.0;component/playernamewindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\PlayerNameWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -87,7 +87,7 @@ namespace Battleship {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.13.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.9.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
