cbc3b11d23031c9b262eacc2e6c946fa07ffbc20
Péter Hegedűs <hpeti0912@gmail.com>
2022-01-07 10:25:59 +0100

    Created layout style for ShipPlacement and empty methods in cs file.

---
+++ Battleship/Battleship/ShipPlacement.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Battleship
+{
+    /// <summary>
+    /// Interaction logic for ShipPlacement.xaml
+    /// </summary>
+    public partial class ShipPlacement : Window
+    {
+        public ShipPlacement()
+        {
+            InitializeComponent();
+        }
+
+        private void shipBtn(object sender, RoutedEventArgs e) //select ship type
+        {
+
+        }
+
+        private void resetBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void rotateBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void randomBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void submitBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void onGridMouseClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void onGridMouseOver(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
