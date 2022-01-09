212fd5f12bc872ca2c3085a4484863c2688e2fa1
Szabó Dániel <daniszabobtw@gmail.com>
2022-01-09 16:43:18 +0100

    AI ship placement finished

--- Battleship/Battleship/AI.cs
+++ Battleship/Battleship/AI.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shapes;
@@ -66,24 +67,24 @@ namespace Battleship
             return x is < 0 or > 9 || y is < 0 or > 9;
         }
 
-        /*private Rectangle CreateShip(int shipSize)
+        public static Rectangle CreateShip(int shipSize, Grid playfield)
         {
             Rectangle ship = new()
             {
-                Fill = Brushes.AliceBlue
+                Fill = Brushes.BlanchedAlmond
             };
 
-            double x = rightTable.Height / 10;
-            double y = rightTable.Width / 10;
+            double x = playfield.Height / 10;
+            double y = playfield.Width / 10;
 
             ship.Height = x;
             ship.Width = y;
-            //ship.Visibility = Visibility.Hidden;
+            ship.Visibility = Visibility.Hidden;
 
             return ship;
         }
 
-        private void GenerateAItable(Random rnd, )
+        public static void GenerateAItable(Random rnd, char[,] aiTable, Grid playfield)
         {
             int orient;
             int randomX;
@@ -95,14 +96,14 @@ namespace Battleship
                 empty = false;
                 orient = rnd.Next(0, 2);
 
-                if (orient == 0) //Függőleges
+                if (orient == 0) //Vertical
                 {
                     randomX = rnd.Next(0, 10);
                     randomY = rnd.Next(0, 10 - i + 1);
 
                     while (empty == false)
                     {
-                        if ((randomY != 0 && char.IsDigit(_aiTable[randomY - 1, randomX])) || (randomY + i - 1 != 9 && char.IsDigit(_aiTable[randomY + i, randomX])))
+                        if ((randomY != 0 && char.IsDigit(aiTable[randomY - 1, randomX])) || (randomY + i - 1 != 9 && char.IsDigit(aiTable[randomY + i, randomX])))
                         {
                             randomX = rnd.Next(0, 10);
                             randomY = rnd.Next(0, 10 - i + 1);
@@ -111,7 +112,7 @@ namespace Battleship
                         {
                             for (int j = 0; j < i; j++)
                             {
-                                if (char.IsDigit(_aiTable[randomY + j, randomX]) || (randomX != 0 && char.IsDigit(_aiTable[randomY + j, randomX - 1])) || (randomX != 9 && char.IsDigit(_aiTable[randomY + j, randomX + 1])))
+                                if (char.IsDigit(aiTable[randomY + j, randomX]) || (randomX != 0 && char.IsDigit(aiTable[randomY + j, randomX - 1])) || (randomX != 9 && char.IsDigit(aiTable[randomY + j, randomX + 1])))
                                 {
                                     randomX = rnd.Next(0, 10);
                                     randomY = rnd.Next(0, 10 - i + 1);
@@ -128,21 +129,58 @@ namespace Battleship
 
                     for (int row = 0; row < i; row++)
                     {
-                        Rectangle ship = CreateShip(i);
+                        Rectangle ship = CreateShip(i, playfield);
 
                         Grid.SetColumn(ship, row + randomY);
                         Grid.SetRow(ship, randomX);
 
-                        _aiTable[randomY + row, randomX] = Convert.ToChar(i.ToString());
-                        rightTable.Children.Add(ship);
+                        aiTable[randomY + row, randomX] = Convert.ToChar(i.ToString());
+                        playfield.Children.Add(ship);
                     }
                 }
-                else //Vízszintes
+                else //Horizontal
                 {
-                    randomX = rnd.Next(0, 10);
-                    randomY = rnd.Next(0, 10 - i + 1);
+                    randomX = rnd.Next(0, 10 - i + 1);
+                    randomY = rnd.Next(0, 10);
+
+                    while (empty == false)
+                    {
+                        if ((randomX != 0 && char.IsDigit(aiTable[randomY, randomX - 1])) || ((randomX + i - 1) != 9 && char.IsDigit(aiTable[randomY, randomX + i])))
+                        {
+                            randomX = rnd.Next(0, 10 - i + 1);
+                            randomY = rnd.Next(0, 10);
+                        }
+                        else
+                        {
+                            for (int j = 0; j < i; j++)
+                            {
+                                if (char.IsDigit(aiTable[randomY, randomX + j]) || (randomY != 0 && char.IsDigit(aiTable[randomY - 1, randomX + j])) || (randomY != 9 && char.IsDigit(aiTable[randomY + 1, randomX + j])))
+                                {
+                                    randomX = rnd.Next(0, 10 - i + 1);
+                                    randomY = rnd.Next(0, 10);
+
+                                    break;
+                                }
+                                else if (j == (i - 1))
+                                {
+                                    empty = true;
+                                }
+                            }
+                        }
+                    }
+
+                    for (int col = 0; col < i; col++)
+                    {
+                        Rectangle ship = CreateShip(i, playfield);
+
+                        Grid.SetColumn(ship, randomY);
+                        Grid.SetRow(ship, col + randomX);
+
+                        aiTable[randomY, col + randomX] = Convert.ToChar(i.ToString());
+                        playfield.Children.Add(ship);
+                    }
                 }
             }
-        }*/
+        }
     }
 }
